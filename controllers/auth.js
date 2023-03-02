88346b1414a70adc9fc147b505fcb188d194122b
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-03-02 10:29:57 -0300

    :-( idk

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,11 +26,12 @@ const login = async (req, res) => {
   if (!user) {
     throw new UnauthenticatedError("Invalid Credentials");
   }
+  // compare password
   const isPasswordCorrect = await user.comparePassword(password);
   if (!isPasswordCorrect) {
     throw new UnauthenticatedError("Invalid Credentials");
   }
-  // compare password
+
   const token = user.createJWT();
   res.status(StatusCodes.OK).json({
     user: {
